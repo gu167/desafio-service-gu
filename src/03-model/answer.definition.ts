caed122f875d5b84725348ffd3b906f8b4f82c45
gu167 <gumendoncabh@gmail.com>
2023-03-20 16:26:25 -0300

    add answer definition

--- src/03-model/answer.definition.ts
+++ src/03-model/answer.definition.ts
@@ -1,3 +1,41 @@
-import { Length } from 'class-validator';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsObject,
+  IsUUID,
+  validateSync,
+} from 'class-validator';
 
-export class AnswerDefinition {}
+export class AnswerDefinition {
+  @IsUUID()
+  id: string;
+
+  @IsNotEmpty()
+  @IsObject()
+  answerContent: object;
+
+  @IsInt()
+  typeAnswer: number;
+
+  constructor(id: string, answerContent: object, typeAnswer: number) {
+    this.id = id;
+    this.answerContent = answerContent;
+    this.typeAnswer = typeAnswer;
+    this.validator();
+  }
+  public clone() {
+    return new AnswerDefinition(this.id, this.answerContent, this.typeAnswer);
+  }
+  private validator() {
+    const validation = validateSync(this);
+
+    if (validation.length) {
+      const errors =
+        validation
+          .map(({ constraints }) => Object.values(constraints).join('; '))
+          .join('; ') + ';';
+
+      throw new Error(`Task Errors:${errors}`);
+    }
+  }
+}

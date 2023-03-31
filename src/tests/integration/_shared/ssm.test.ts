59d11768c2f38308a164aa127f1f4fb51a188415
gu167 <gumendoncabh@gmail.com>
2023-03-31 15:52:24 -0300

    fix: ssm test

--- src/tests/integration/_shared/ssm.test.ts
+++ src/tests/integration/_shared/ssm.test.ts
@@ -2,10 +2,9 @@ import { SsmProvider } from 'src/_shared/ssm';
 
 describe('#SssProvider - Integration Tests', () => {
   test('#getParametersByPrefix - should get parameters by prefix', async () => {
-    const { TABLE_NAME } = await SsmProvider.getParametersByPrefix(
+    const { TEST_PARAM } = await SsmProvider.getParametersByPrefix(
       'desafio-infra-gu',
     );
-    console.log(TABLE_NAME);
-    expect(TABLE_NAME).toBe('desafio-infra-gu-dev-table');
+    expect(TEST_PARAM).toBe('test');
   });
 });
